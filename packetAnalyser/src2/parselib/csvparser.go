1767612e1a9f3418ebd33c4f8596f00e9eb45444
Your Name <chrislew@stud.ntnu.no>
2024-04-04 11:17:52 +0000

    add found_match to packet struct and csv

--- packetAnalyser/src2/parselib/csvparser.go
+++ packetAnalyser/src2/parselib/csvparser.go
@@ -17,11 +17,13 @@ type Packet struct {
 	Encapsulated_psize int
 	Rx_ts              float64 // Format: seconds with nanosecond precision
 	Tx_ts              float64 // Format: seconds with nanosecond precision
+	Found_match        bool
 }
 
 // ConvertToCSVFormat returns a comma-separated string without whitespace to
 // be used in a csv-file. The order of the values are
 // 1. Srcip 2. Dstip 3. Psize 4. Encapsulated_psize 5. Rx_ts 6. Tx_ts
+// 7. Found_match
 func (packet Packet) ConvertToCSVFormat() string {
 	txSec := int64(packet.Tx_ts)
 	txNanosec := int64(math.Mod(packet.Tx_ts, 1) * math.Pow10(9))
@@ -30,7 +32,15 @@ func (packet Packet) ConvertToCSVFormat() string {
 	rxNanosec := int64(math.Mod(packet.Rx_ts, 1) * math.Pow10(9))
 	rxTime := time.Unix(rxSec, rxNanosec)
 
-	values := []string{packet.Srcip, packet.Dstip, strconv.Itoa(packet.Psize), strconv.Itoa(packet.Encapsulated_psize), rxTime.String(), txTime.String()}
+	values := []string{
+		packet.Srcip,
+		packet.Dstip,
+		strconv.Itoa(packet.Psize),
+		strconv.Itoa(packet.Encapsulated_psize),
+		rxTime.String(),
+		txTime.String(),
+		strconv.FormatBool(packet.Found_match),
+	}
 
 	return strings.Join(values, ",")
 }
@@ -39,7 +49,7 @@ func (packet Packet) ConvertToCSVFormat() string {
 // Packet-object and returns a pointer to said object. It also accepts
 // a timestamp layout to parse the timestamp from the record.
 // It assumes the order of the columns to be 1. Srcip 2. Dstip 3. Psize
-// 4. Encapsulated_psize 5. Rx_ts 6. Tx_ts
+// 4. Encapsulated_psize 5. Rx_ts 6. Tx_ts 7. Found_match
 func parseCSVRecordToPacket(record []string, timestampLayout string) (*Packet, error) {
 	srcip := record[0]
 	dstip := record[1]
@@ -47,6 +57,7 @@ func parseCSVRecordToPacket(record []string, timestampLayout string) (*Packet, e
 	encapsulated_psize := record[3]
 	rx_ts := record[4]
 	tx_ts := record[5]
+	found_match := record[6]
 
 	psize_int, err := strconv.Atoi(psize)
 	if err != nil {
@@ -66,7 +77,20 @@ func parseCSVRecordToPacket(record []string, timestampLayout string) (*Packet, e
 		return nil, err
 	}
 
-	packet := Packet{srcip, dstip, psize_int, encapsulated_psize_int, convertNanosecondsToSeconds(rx_timestamp.UnixNano()), convertNanosecondsToSeconds(tx_timestamp.UnixNano())}
+	found_match_bool, err := strconv.ParseBool(found_match)
+	if err != nil {
+		return nil, err
+	}
+
+	packet := Packet{
+		srcip,
+		dstip,
+		psize_int,
+		encapsulated_psize_int,
+		convertNanosecondsToSeconds(rx_timestamp.UnixNano()),
+		convertNanosecondsToSeconds(tx_timestamp.UnixNano()),
+		found_match_bool,
+	}
 
 	return &packet, nil
 }
