2a962f19989bf06d74200df5132ff0a4b939c1bf
Sebastian-Fuglesang <sebastian.fuglesang@hotmail.com>
2024-04-26 07:33:23 +0000

    improve syncing and perform test for more pps values

--- validation/automate_validation_bridge.sh
+++ validation/automate_validation_bridge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of values
-values=(1000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000)
+values=(1000 10000 20000 30000 40000 50000 60000 70000 80000 90000  110000 120000 130000 140000 150000)
 
 # Iterate over the values and run Ansible playbook
 for value in "${values[@]}"; do
