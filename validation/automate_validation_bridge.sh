e173f8d1302b135049d7183f25e031b088a0422f
Sebastian-Fuglesang <sebastian.fuglesang@hotmail.com>
2024-05-09 12:13:46 +0000

    plots

--- validation/automate_validation_bridge.sh
+++ validation/automate_validation_bridge.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-pps_rates=(1000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000 110000 120000 130000 140000 150000 160000 170000 180000 190000 200000)
-
-# Iterate over the values and run Ansible playbook
-for pps in "${pps_rates[@]}"; do
-    echo "Running Ansible playbook for value: $value"
-    ansible-playbook validate_bridge.yml --extra-vars "pps=$pps"
+pps_rates=(10000 20000 30000 40000 50000 60000 70000 80000 90000 100000)
+payload_sizes=(16 426 1432)
+for payload_size in "${payload_sizes[@]}"; do
+    echo "Running trials for payload size: $payload_size"
+    # Iterate over the values and run Ansible playbook
+    for pps in "${pps_rates[@]}"; do
+        echo "Running Ansible playbook for value: $pps"
+        ansible-playbook validate_bridge.yml --extra-vars "pps=$pps payload_size=$payload_size"
+    done
 done
